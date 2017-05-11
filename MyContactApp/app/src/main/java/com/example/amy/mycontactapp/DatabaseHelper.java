17b6c1ee64c0e5d445f3d8b0e0bd910526f4482f
luoa2945 <AmyLuo1999@gmail.com>
2017-05-11 10:49:18 -0700

    Create database helper class

---
+++ MyContactApp/app/src/main/java/com/example/amy/mycontactapp/DatabaseHelper.java
@@ -0,0 +1,32 @@
+package com.example.amy.mycontactapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.StrictMode;
+
+/**
+ * Created by Amy on 5/11/2017.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "Contact.db"; //constant
+    public static final String TABLE_NAME = "contact_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
