158170322e56bebcc128d874346516cb6a217a60
luoa2945 <AmyLuo1999@gmail.com>
2017-05-16 11:06:40 -0700

    Created variables for 2 other fields. Added parameters to inserData. Started on viewData method.

--- MyContactApp/app/src/main/java/com/example/amy/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/amy/mycontactapp/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.amy.mycontactapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.StrictMode;
@@ -14,8 +15,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "Contact.db"; //constant
     public static final String TABLE_NAME = "contact_table";
-    public static final String COL_1 = "ID";
-    public static final String COL_2 = "NAME";
+    //public static final String COL_1 = "ID";
+    public static final String COL_1 = "NAME";
+    public static final String COL_2 = "NUMBER: ";
+    public static final String COL_3 = "AGE: ";
+
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -32,13 +36,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public boolean insertData(String name){
+    public boolean insertData(String name, String number, String age){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_2, name);
+        contentValues.put(COL_1, name);
+        //contentValues.put(COL_2, number);
+        //contentValues.put(COL_3, age);
 
         long result = db.insert(TABLE_NAME, null, contentValues);
         if(result == -1) return false;
         else return true;
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
     }
 }
--- MyContactApp/app/src/main/java/com/example/amy/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/amy/mycontactapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.amy.mycontactapp;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,7 +14,10 @@ public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper myDb;
     EditText editName;
+    EditText editNumber;
+    EditText editAge;
     Button btnAddData;
+    Button showData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
         //Add the layout vars
         editName = (EditText) findViewById(R.id.editText_Name);
+        editNumber = (EditText) findViewById(R.id.editText_Number);
+        editAge = (EditText) findViewById(R.id.editText_Age);
     }
 
     public void addData(View v){
-        boolean isInserted = myDb.insertData(editName.getText().toString());
+        boolean isInserted = myDb.insertData(editName.getText().toString(),
+                editNumber.getText().toString(),
+                editAge.getText().toString());
 
         if(isInserted == true){
             Log.d("MyContact", "Success inserting data");
@@ -49,5 +57,25 @@ public class MainActivity extends AppCompatActivity {
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
         }
+    }
+
+    public void viewData(View v){
+        Cursor res = myDb.getAllData();
+        if(res.getCount() == 0) {
+            showMessage("Error", "No data found in database");
+            //output message using Log.d and Toast
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        //set up a loop with Cursor(res) using moveToNext
+        //append each COL to the buffer
+        //display message using showMessage
+        showMessage("Data", buffer.toString());
+    }
+
+    private void showMessage(String title, String message) {
+        //AlertDialog.Builder
+
     }
 }
